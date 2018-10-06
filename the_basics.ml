b1ec2d14dd1af1ee71c44802386deb730f855b5f
KENN <cent.rngttwt@gmail.com>
2018-10-06 20:25:11 +0900

    add Defining a function

--- the_basics.ml
+++ the_basics.ml
@@ -1,9 +1,23 @@
-let () = print_endline "Hello, World"
+let avarage a b =
+    (a +. b) /. 2.0
+;;
 
-(* Comments *)
-(* This is single-line comment. *)
 
-(* This is a
- * multi-line
- * comment.
- *)
+let main () =
+    print_endline "Hello, World";
+
+    (* Comments *)
+    (* This is single-line comment. *)
+
+    (* This is a
+     * multi-line
+     * comment.
+     *)
+
+    (* Calling functions *)
+    (* Defining a function *)
+    print_endline (string_of_float (avarage 3.0 4.0));
+
+;;
+
+main()
